963b82863b87b5deab7daf3416d399b54a780916
mru24 <mru24@yahoo.com>
2019-07-17 09:38:57 +0100

    hamburger edited

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
 
-  alert('Ok');
 
+//****************NAVBAR HAMBURGER***********************
 
+var icon = $('nav button.navbar-toggler');
 
+// MOUSEOVER
+$(icon)
+.mouseout(function() {
+  $('span', this).removeClass('stage1');
+})
+
+.mouseover(function() {
+  $('span', this).addClass('stage1');
+});
+
+// CLICK
+$(icon).click(function() {
+  if($('span', this).hasClass('stage2')) {
+    $('span', this).removeClass('stage3');
+    $('span', this).removeClass('stage2');
+  } else {
+    $('span', this).addClass('stage2');
+    setTimeout(function() {
+      $('span.navbar-toggler-icon').addClass('stage3');
+    }, 1200);
+  }
+});
+//****************NAVBAR HAMBURGER END***********************
 
 
 
